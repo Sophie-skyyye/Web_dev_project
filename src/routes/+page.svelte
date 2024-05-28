aa603b8e10b9c0f96298828954a23e3e9b660adf
Sophie Dickerson <sophieskyed@gmail.com>
2024-05-28 22:17:29 +1200

    added a logo and nav links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <section class="header">
     <nav>
-
+        <a href="index.html"><img src="Images/logo3.jpg" class="circle-image"></a>  
+        <div class="links">
+          <ul>
+              <li><a href="">HOME</a></li>
+              <li><a href="">ABOUT</a></li>
+              <li><a href="">COURSES</a></li>
+              <li><a href="">BLOG</a></li>
+              <li><a href="">CONTACT</a></li>
+          </ul>
+        </div>
     </nav>
 </section>
